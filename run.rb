4116ce35a36962f3abc0bd760f32444259875a62
Josh Cheek <josh.cheek@gmail.com>
2016-12-30 17:03:00 -0600

    Partially rewritten to use spawn
    It fails on "cleans up the process and all its children after a timeout"
    because it leaves an orphan process:
    
    $ ps aux | grep ruby | grep -v grep
    josh            20855   0.0  0.3  2477868  21520 s010  S     5:01PM   0:00.22 ruby -e puts $$; $stdout.flush; sleep

--- run.rb
+++ run.rb
@@ -1,61 +1,72 @@
-require 'childprocess'
+require 'timeout'
 
 Result = Struct.new :code, :stdout, :stderr, :pid, :timed_out do
   alias timed_out? timed_out
 end
 
-def run(stdin:, program:, argv:, timeout: nil, write_stdout:nil, write_stderr:nil)
+# what is the differnce between the group id and the effective group id?
+
+def run(stdin:, program:, argv:, timeout: 0, write_stdout:nil, write_stderr:nil)
+  result = Result.new
+
+  # setup pipes
   read_stdout, write_stdout = IO.pipe unless write_stdout
   read_stderr, write_stderr = IO.pipe unless write_stderr
-  child = ChildProcess.build program, *argv
-  child.leader    = true
-  child.duplex    = true
-  child.io.stdout = write_stdout
-  child.io.stderr = write_stderr
-  child.start
-  write_stdout.close if read_stdout
-  write_stderr.close if read_stderr
-  result = Result.new
-  result.pid = child.pid
+  read_stdin,  write_stdin  = IO.pipe
   # child.io.stdin.binmode
   # child.io.stdin.sync = true
-  stdin.each_char { |c| child.io.stdin.write c }
-  child.io.stdin.close
-  if timeout
-    puts "WAITING"
-    child.poll_for_exit(timeout)
-  else
-    child.wait
+
+  # spawn
+  # child.leader    = true
+  result.pid = spawn program, *argv, in: read_stdin, out: write_stdout, err: write_stderr
+
+  # close pipes in parent so that child has the last open handle
+  # thus closing them in the child closes them completely
+  read_stdin.close
+  write_stdout.close if read_stdout
+  write_stderr.close if read_stderr
+
+  # send standard input
+  stdin.each_char { |c| write_stdin.write c }
+  write_stdin.close
+
+  # wait for result, timeout if it takes too long
+  Timeout.timeout timeout do
+    Process.wait result.pid
+    result.code = $?.exitstatus
   end
-  child.exit_code
-rescue ChildProcess::TimeoutError
+
+rescue Timeout::Error
   puts "TIMED OUT"
   result.timed_out = true
+  Process.kill 'KILL', result.pid
+  Process.wait result.pid
+  result.code = $?.exitstatus
 ensure
-  if child.stop
-    puts "GETTING CODE"
-    result.code = child.exit_code
-  else
-    puts "Using a code of 1"
-    result.code = 1
-  end
+  # if child.stop
+  #   puts "GETTING CODE"
+  #   result.code = child.exit_code
+  # else
+  #   puts "Using a code of 1"
+  #   result.code = 1
+  # end
   result.stdout = read_and_close(read_stdout)
   result.stderr = read_and_close(read_stderr)
-  return result
+  return result unless $!
 end
 
 def read_and_close(stream)
   return "" unless stream
-  read = ""
+  printed = ""
   loop do
     readable, _ = IO.select [stream], [$stdout]
     break unless readable.any?
     begin
-      read += stream.readpartial(1)
+      printed += stream.readpartial(1)
     rescue EOFError
       break
     end
   end
   stream.close
-  read
+  printed
 end
